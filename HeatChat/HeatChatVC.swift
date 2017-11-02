14d68393b687ecc7b67d5d77c9b391c170ffd339
schlaBAM <brett.morrison@live.com>
2017-11-01 22:03:57 -0700

    chatview / scrollview adjust accordingly with keyboard showing/hiding, couple other small changes

--- HeatChat/HeatChatVC.swift
+++ HeatChat/HeatChatVC.swift
@@ -14,18 +14,22 @@ import FirebaseDatabase
 class HeatChatVC: UIViewController, UITextViewDelegate{
 
     @IBOutlet weak var messageView: UIScrollView!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var chatBox: UITextView!
+    @IBOutlet weak var chatView: UIView!
+    @IBOutlet weak var stackView: UIStackView!
     
     let appDel = UIApplication.shared.delegate as! AppDelegate
     var messages = [Message]()
     var messageViews = [UIView]()
-    @IBOutlet weak var sendButton: UIButton!
-    @IBOutlet weak var chatBox: UITextView!
     var yHeight: CGFloat = 0.0
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardIsShowing(_:)), name: Notification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardIsHiding(_:)), name: Notification.Name.UIKeyboardWillHide, object: nil)
 //        Database.database().reference().child("messages").observe(DataEventType.childAdded) { (data) in
 //            guard let data = data.value as? [String : Any] else {return}
 //            self.createChatMessage(data: data)
@@ -43,6 +47,12 @@ class HeatChatVC: UIViewController, UITextViewDelegate{
     override func viewWillAppear(_ animated: Bool) {
         loadMessages()
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self, name: Notification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: Notification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -58,50 +68,47 @@ class HeatChatVC: UIViewController, UITextViewDelegate{
     
     func createChatMessage(data : [String : Any]){
         
-        DispatchQueue.main.async {
-            
-            let context = self.appDel.persistentContainer.viewContext
-            let chatMessage = Message(context: context)
-    
-            chatMessage.text = (data["text"] as! String).trimmingCharacters(in: .newlines)
-            chatMessage.uid = data["uid"] as! String
-            chatMessage.lat = data["lat"] as! Double
-            chatMessage.lon = data["lon"] as! Double
-            chatMessage.time = data["time"] as! Int64
-            
-            self.messages.append(chatMessage)
-            
-            //message from other user
-            var textLabel = UITextView(frame: CGRect(x: self.view.bounds.width * 0.05, y: self.yHeight, width: self.view.bounds.width * 0.45, height: self.view.bounds.height * 0.125))
-            textLabel.backgroundColor = UIColor(red: 0.6078, green: 1, blue: 0.7373, alpha: 1.0) /* #9bffbc */
-            textLabel.text = chatMessage.text
-            textLabel.sizeToFit()
+        let context = appDel.persistentContainer.viewContext
+        let chatMessage = Message(context: context)
+
+        chatMessage.text = (data["text"] as! String).trimmingCharacters(in: .newlines)
+        chatMessage.uid = data["uid"] as! String
+        chatMessage.lat = data["lat"] as! Double
+        chatMessage.lon = data["lon"] as! Double
+        chatMessage.time = data["time"] as! Int64
+        
+        messages.append(chatMessage)
+        
+        //message from other user
+        var textLabel = UITextView(frame: CGRect(x: view.bounds.width * 0.05, y: yHeight, width: view.bounds.width * 0.45, height: view.bounds.height * 0.125))
+        textLabel.backgroundColor = UIColor(red: 0.6078, green: 1, blue: 0.7373, alpha: 1.0) /* #9bffbc */
+        textLabel.text = chatMessage.text
+        textLabel.sizeToFit()
 
 //                UIColor(red: 0, green: 0.9176, blue: 0.5176, alpha: 1.0) /* #00ea84 */
 
-            if self.defaults.string(forKey: "userID") == chatMessage.uid {
-                textLabel = UITextView(frame: CGRect(x: self.view.bounds.width * 0.5, y: self.yHeight, width: self.view.bounds.width * 0.45, height: self.view.bounds.height * 0.125))
-                textLabel.textAlignment = .right
-                textLabel.backgroundColor = UIColor(red: 0.298, green: 0.8706, blue: 1, alpha: 1.0) /* #4cdeff */
-                textLabel.text = chatMessage.text
-                textLabel.sizeToFit()
-                textLabel.center.x = self.view.bounds.width*0.95 - textLabel.bounds.width * 0.5
-            }
-            
-            textLabel.layer.cornerRadius = 10
-            textLabel.layer.borderColor = UIColor.black.cgColor
-            textLabel.layer.borderWidth = 1
-            textLabel.isUserInteractionEnabled = false
-            self.messageView.addSubview(textLabel)
-            
-            self.messageViews.append(textLabel)
-            
-            self.yHeight += textLabel.bounds.height * 1.2
-        
-            self.messageView.contentSize.height = self.yHeight
-
-            self.messageView.scrollRectToVisible((self.messageViews.last?.frame)!, animated: true)
+        if defaults.string(forKey: "userID") == chatMessage.uid {
+            textLabel = UITextView(frame: CGRect(x: view.bounds.width * 0.5, y: yHeight, width: view.bounds.width * 0.45, height: view.bounds.height * 0.125))
+            textLabel.backgroundColor = UIColor(red: 0.298, green: 0.8706, blue: 1, alpha: 1.0) /* #4cdeff */
+            textLabel.text = chatMessage.text
+            textLabel.sizeToFit()
+            textLabel.center.x = view.bounds.width*0.95 - textLabel.bounds.width * 0.5
         }
+        
+        textLabel.layer.cornerRadius = 10
+        textLabel.layer.borderColor = UIColor.black.cgColor
+        textLabel.layer.borderWidth = 1
+        textLabel.isUserInteractionEnabled = false
+        messageView.addSubview(textLabel)
+        
+        messageViews.append(textLabel)
+        
+        yHeight += textLabel.bounds.height * 1.2
+    
+        messageView.contentSize.height = yHeight        
+        messageView.scrollRectToVisible(messageView.frame, animated: true)
+
+        messageView.scrollRectToVisible((messageViews.last?.frame)!, animated: true)
 
     }
     
@@ -115,6 +122,25 @@ class HeatChatVC: UIViewController, UITextViewDelegate{
 //        }
     }
     
+    @objc func keyboardIsShowing(_ notification : Notification){
+        chatBox.text = ""
+        if let keyboardFrame: NSValue = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRect = keyboardFrame.cgRectValue
+            stackView.frame.origin.y -= keyboardRect.height
+            messageView.scrollRectToVisible((messageViews.last?.frame)!, animated: true)
+        }
+    }
+    
+    @objc func keyboardIsHiding(_ notification : Notification){
+        if let keyboardFrame: NSValue = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRect = keyboardFrame.cgRectValue
+            stackView.frame.origin.y += keyboardRect.height
+        }
+        if chatBox.text == ""{
+            chatBox.text = "Add a message.."
+        }
+    }
+    
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         if (text == "\n") {
             textView.resignFirstResponder()
@@ -125,10 +151,7 @@ class HeatChatVC: UIViewController, UITextViewDelegate{
     
 
     @IBAction func sendTapped(_ sender: Any) {
-        print("tapped bitch")
         let message = ["lat" : defaults.double(forKey: "userLat"), "lon" : defaults.double(forKey: "userLon"), "text" : chatBox.text, "time" : Int64(NSDate().timeIntervalSince1970), "uid" : defaults.string(forKey: "userID")!] as [String : Any]
-        print(Auth.auth().currentUser?.uid)
-        print("message: \(message)")
         Database.database().reference().child("messages").childByAutoId().setValue(message)
         
         chatBox.text = "Add a message.."
