39ad5e42741832eb997f68e160d0d92e2c1cf6ab
schlaBAM <brett.morrison@live.com>
2017-11-19 21:46:05 -0800

    thought I could restart sidebarview tonight but I'm so damn tired

---
+++ HeatChat/Views/SideBarView.swift
@@ -0,0 +1,29 @@
+//
+//  SideBarView.swift
+//  HeatChat
+//
+//  Created by BAM on 2017-11-19.
+//  Copyright © 2017 BAM. All rights reserved.
+//
+
+import UIKit
+
+class SideBarView: UITableView {
+    
+    override init(frame: CGRect, style: UITableViewStyle) {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
