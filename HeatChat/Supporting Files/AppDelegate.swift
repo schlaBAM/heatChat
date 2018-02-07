bc910e4e9073df7bb57e3e684aad009c2e52837d
schlaBAM <brett.morrison@live.com>
2018-02-07 15:36:58 -0800

    build info, final changes.

--- HeatChat/Supporting Files/AppDelegate.swift
+++ HeatChat/Supporting Files/AppDelegate.swift
@@ -55,9 +55,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
-//        if let uni = selectedUni, let views = numberViewers {
-//            changeUsers(with: views, and: uni, leavingApp: true)
-//        }
         self.saveContext()
     }
     
